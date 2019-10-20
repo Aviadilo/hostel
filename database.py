09bc82143224ec8ae97e2b8a53685a574aec4dec
Hanna Lychauka <zuzga@tut.by>
2019-10-20 12:32:01 +0300

    removed duplicated class QueryBodies from module database

--- database.py
+++ database.py
@@ -55,36 +55,6 @@ class TableWriter(Connection):
         operators = operators[:-1]
         return operators
 
-class QueryBodies:
-    count_students_in_rooms = "SELECT r.id, r.name, COUNT(s.room) " \
-                              "FROM rooms r " \
-                              "LEFT JOIN students s ON r.id = s.room " \
-                              "GROUP BY s.room " \
-                              "ORDER BY s.room "
-
-    find_the_yougest_age = "SELECT r.id, r.name, " \
-                           "AVG((YEAR(CURRENT_TIMESTAMP(0)) - YEAR(s.birthday)) - " \
-                           "(RIGHT(CURRENT_TIMESTAMP(0),5)<RIGHT(s.birthday,5))) AS age " \
-                           "FROM rooms r " \
-                           "LEFT JOIN students s ON r.id = s.room " \
-                           "GROUP BY s.room " \
-                           "HAVING age IS NOT NULL " \
-                           "ORDER BY age " \
-                           "LIMIT 5"
-
-    find_the_biggest_age_diff = "SELECT r.id, r.name, (MAX(YEAR(s.birthday))-MIN(YEAR(s.birthday))) AS age_diff " \
-                                " FROM rooms r " \
-                                "LEFT JOIN students s ON r.id = s.room " \
-                                "GROUP BY s.room " \
-                                "ORDER BY age_diff DESC " \
-                                "LIMIT 5"
-
-    find_rooms_with_both_male_female = "SELECT r.id, r.name FROM rooms r " \
-                                       "LEFT JOIN students s ON r.id = s.room " \
-                                       "WHERE s.sex IN ('F','M') " \
-                                       "GROUP BY s.room " \
-                                       "HAVING COUNT(DISTINCT s.sex) = 2 " \
-                                       "ORDER BY s.room"
 
 class QueryMaker(Connection):
     """Class executes database queries"""
